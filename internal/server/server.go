62c9add796d70cd3e38c459594609f448db9547d
Alperen Ağa <alperencode@gmail.com>
2024-09-10 00:40:44 +0300

    chore: add inital structs and constructor methods

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,27 @@
 package server
+
+import (
+	"net"
+	"sync"
+)
+
+type Server struct {
+	listener net.Listener
+	clients  map[string]net.Conn
+	mu       sync.Mutex
+}
+
+func NewServer(address string) (*Server, error) {
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+
+	server := Server{
+		listener: listener,
+		clients:  make(map[string]net.Conn),
+		mu:       sync.Mutex{},
+	}
+
+	return &server, nil
+}
